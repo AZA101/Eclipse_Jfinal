4a1c0c6676d6331de7af898619e77b5c4b3b0846
AZA101 <50098224+AZA101@users.noreply.github.com>
2019-05-16 17:35:43 +0800

    Update BlogController.java
    新增

--- Eclipse_Idea/src/demo/BlogController.java
+++ Eclipse_Idea/src/demo/BlogController.java
@@ -5,6 +5,7 @@ import com.jfinal.core.Controller;
 import model.Blog;
 // 远程的部分增加内容
 //定义一个类去继承，测试上传，远程也添加注释
+//新增的
 public class BlogController extends Controller {
 	  public void save() {
 		  //aaa
