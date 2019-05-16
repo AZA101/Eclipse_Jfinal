f700ee5d554879a940d6054b1afc65041b7d7fcd
yjb <1404739943@qq.com>
2019-05-16 09:25:16 +0800

    增加注释

--- Eclipse_Idea/src/demo/BlogController.java
+++ Eclipse_Idea/src/demo/BlogController.java
@@ -3,10 +3,11 @@ package demo;
 import com.jfinal.core.Controller;
 
 import model.Blog;
-
+//定义一个类去继承
 public class BlogController extends Controller {
 	  public void save() {
 		  Blog blog=getModel(Blog.class);
 		  blog=getModel(Blog.class,"otherName");
 	  }
+	  
 }
