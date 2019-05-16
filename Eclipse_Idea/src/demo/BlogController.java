16a64a1afe4a5d8cd1981f449032ba2028b9cb77
yjb <1404739943@qq.com>
2019-05-16 09:26:30 +0800

    再次增加注释

--- Eclipse_Idea/src/demo/BlogController.java
+++ Eclipse_Idea/src/demo/BlogController.java
@@ -3,7 +3,7 @@ package demo;
 import com.jfinal.core.Controller;
 
 import model.Blog;
-//定义一个类去继承
+//定义一个类去继承，测试上传
 public class BlogController extends Controller {
 	  public void save() {
 		  Blog blog=getModel(Blog.class);
